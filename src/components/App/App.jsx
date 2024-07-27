82cb807353d730fe25eda0f36be038e031d6e1d2
dr-jeky11 <tyrsyna@gmail.com>
2024-07-27 11:13:40 +0200

    init

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,3 @@
-import userData from '../../userData.json'
-import friends from '../../friends.json'
-import transactions from '../../transactions.json'
-
-import Profile from '../Profile/Profile';
-import FriendList from '../FriendList/FriendList';
-import TransactionHistory from '../TransactionHistory/TransactionHistory'
-
 const App = () => {
   return (
     <>
