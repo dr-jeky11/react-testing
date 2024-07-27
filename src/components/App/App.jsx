e7c86f57ae554fa391d21911cf432f8c0e5b2d55
dr-jeky11 <tyrsyna@gmail.com>
2024-07-27 12:28:32 +0200

    module 4 testing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,26 @@
-const App = () => {
+import { useRef } from "react";
+
+const Player = ({ source }) => {
+  const playerRef = useRef();
+
+  const play = () => playerRef.current.play();
+
+  const pause = () => playerRef.current.pause();
+
   return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-
-      <FriendList friends={friends} />
-
-      <TransactionHistory items={transactions} />
-    </>
+    <div>
+      <video ref={playerRef} src={source}>
+        Sorry, your browser does not support embedded videos.
+      </video>
+      <div>
+        <button onClick={play}>Play</button>
+        <button onClick={pause}>Pause</button>
+      </div>
+    </div>
   );
 };
 
+const App = () => {
+  return <Player source="<http://media.w3.org/2010/05/sintel/trailer.mp4>" />;
+};
 export default App;
